ff5d3d419ce5b066a65e0fda28f625c289072780
sonbarse17 <sushantsonbarse90@gmail.com>
2026-02-15 20:46:59 +0530

    fix: correct subnet CIDR ranges to match VPC CIDR block

--- infrastructure/modules/aws/vpc/variables.tf
+++ infrastructure/modules/aws/vpc/variables.tf
@@ -29,13 +29,13 @@ variable "availability_zones" {
 variable "public_subnet_cidrs" {
   description = "List of CIDR blocks for public subnets"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+  default     = ["10.1.1.0/24", "10.1.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   description = "List of CIDR blocks for private subnets"
   type        = list(string)
-  default     = ["10.0.10.0/24", "10.0.11.0/24"]
+  default     = ["10.1.10.0/24", "10.1.11.0/24"]
 }
 
 variable "cluster_name" {
